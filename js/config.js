74b43cbabd8cfcb5bd02d965fb114afd2b492b46
Thilina Dilanka Jayasuriya <65086051+thilinadj@users.noreply.github.com>
2025-07-20 01:35:43 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_cFoegjyYO', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3djqo0eck8qmgjm5etkgi40gu8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_PrdOrovzd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4e01ag7lf30sc4vpmiro3im5la', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
